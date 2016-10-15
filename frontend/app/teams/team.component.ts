7b8780558ee977c4114706b9cc56521215ba71d4
MetaDark <kurt.bruneau@gmail.com>
2016-10-15 12:04:53 -0400

    Added team components

---
+++ frontend/app/teams/team.component.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+    moduleId: module.id,
+    selector: 'app-team',
+    templateUrl: 'team.component.html',
+})
+
+export class TeamComponent { }
