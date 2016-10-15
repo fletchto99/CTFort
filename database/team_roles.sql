c34599894c6ea16fe15b934b83fb54a2add45bab
Matt Langlois <fletchto99@gmail.com>
2016-10-15 11:16:08 -0400

    Insert default roles

--- database/team_roles.sql
+++ database/team_roles.sql
@@ -2,3 +2,6 @@ CREATE TABLE team_roles(
     Team_Role_ID SERIAL NOT NULL PRIMARY KEY,
     Role_Name varchar(30) NOT NULL
 );
+
+INSERT INTO team_roles(Role_Name) VALUES ('Administrator');
+INSERT INTO team_roles(Role_Name) VALUES ('Member');
