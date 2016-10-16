6d8ac2895f953b97b1f892754e4f1ca4704d5a75
RyanAbraham <ryan_ja@live.ca>
2016-10-16 01:36:20 -0400

    Added modals and fixed angular linking for teams and modules pages

--- frontend/app/teams/module.component.ts
+++ frontend/app/teams/module.component.ts
@@ -1,9 +1,19 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
     moduleId: module.id,
-    selector: 'app-team',
+    selector: 'app-module',
     templateUrl: 'module.component.html',
+    styleUrls: ['module.component.css']
 })
 
-export class ModuleComponent { }
+export class ModuleComponent implements OnInit {
+	ngOnInit() {
+        $('.modal-trigger').leanModal({
+			dismissible: true, // Modal can be dismissed by clicking outside of the modal
+			opacity: .5, // Opacity of modal background
+			in_duration: 300, // Transition in duration
+			out_duration: 200, // Transition out duration
+		});
+	}
+}
