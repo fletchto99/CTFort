f5a48c2720c260cf7749f9067481daa7afd78dbe
MetaDark <kurt.bruneau@gmail.com>
2016-10-16 01:52:09 -0400

    Added logout button

--- frontend/app/teams/team.component.ts
+++ frontend/app/teams/team.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+
+import { AuthService } from '../entry/auth.service';
 
 @Component({
     moduleId: module.id,
@@ -8,12 +11,20 @@ import { Component, OnInit } from '@angular/core';
 })
 
 export class TeamComponent implements OnInit {
-	ngOnInit() {
+    constructor(private authService: AuthService,
+                private router: Router) { }
+
+    ngOnInit() {
         $('.modal-trigger').leanModal({
-			dismissible: true, // Modal can be dismissed by clicking outside of the modal
-			opacity: .5, // Opacity of modal background
-			in_duration: 300, // Transition in duration
-			out_duration: 200, // Transition out duration
-		});
-	}
+            dismissible: true, // Modal can be dismissed by clicking outside of the modal
+            opacity: .5, // Opacity of modal background
+            in_duration: 300, // Transition in duration
+            out_duration: 200, // Transition out duration
+        });
+    }
+
+    logout() {
+        this.authService.logout();
+        this.router.navigate(['/entry/login']);
+    }
 }
