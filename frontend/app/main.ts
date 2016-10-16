9dfd512c964a0673ddbb774f93ec4fcfe561ecba
MetaDark <kurt.bruneau@gmail.com>
2016-10-15 22:27:24 -0400

    Enable production mode

--- frontend/app/main.ts
+++ frontend/app/main.ts
@@ -1,6 +1,9 @@
 import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
+import {enableProdMode} from "angular2/core";
 
 import { AppModule } from './app.module';
+
+enableProdMode();
 
 const platform = platformBrowserDynamic();
 platform.bootstrapModule(AppModule);
